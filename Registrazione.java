78e21ba7c4b07559c40ecd6a8fb2594951733ca8
Mariano Salvatore Librici <42919644+MarianoAkaMery@users.noreply.github.com>
2022-05-10 19:35:04 +0200

    Registrazione Finita!

--- Registrazione.java
+++ Registrazione.java
@@ -1,4 +1,11 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.*;
+import java.io.FileWriter;
+import com.opencsv.CSVWriter;
+
+
+
 
 public class Registrazione {
 
@@ -15,20 +22,22 @@ public class Registrazione {
     }}
 
     //Registration function 
-    public static void RegistrazioneNew(){
+    public static boolean RegistrazioneNew() throws FileNotFoundException{
 
         System.out.println("Benvenuto nella registrazione account di EmotionalSongs\n");
         boolean ContinuaLogin = true;
+        ArrayList<String> LoginSession =new ArrayList<>();
         while(ContinuaLogin==true){
 
             //Inserimento Nome
             try{        
                 System.out.println("Inserisci il tuo nome:");
                 String Name=scan.nextLine();
+                LoginSession.add(Name);
 
             }
             catch(Exception e) {
-                System.out.println("Nome inserito non valido");
+                System.out.println("Nome inserito non valido\n");
                 wait(3500);
                 break;
                 
@@ -38,10 +47,11 @@ public class Registrazione {
             try{        
                 System.out.println("Inserisci il tuo cognome:");
                 String Surname=scan.nextLine();
+                LoginSession.add(Surname);
 
             }
             catch(Exception e) {
-                System.out.println("Cognome inserito non valido");
+                System.out.println("Cognome inserito non valido\n");
                 wait(3500);
                 break;            
             }
@@ -49,9 +59,10 @@ public class Registrazione {
             //Inserimento CodiceFiscale    
             System.out.println("Inserisci il tuo codice fiscale:");
             String FiscalCode=scan.nextLine();
+            LoginSession.add(FiscalCode);
     
             if (FiscalCode.length()!=16){
-                System.out.println("codice fiscale inserito non valido");
+                System.out.println("codice fiscale inserito non valido\n");
                 wait(3500);
                 break;
             } 
@@ -60,10 +71,11 @@ public class Registrazione {
             try{        
                 System.out.println("Inserisci il tuo indirizzo fisico:");
                 String PhysicalAddress=scan.nextLine();
+                LoginSession.add(PhysicalAddress);
     
             }
             catch(Exception e) {
-                System.out.println("Indirizzo fisico inserito non valido");
+                System.out.println("Indirizzo fisico inserito non valido\n");
                 wait(3500);
                 break;
             }
@@ -71,9 +83,11 @@ public class Registrazione {
             //Inserimento Email          
             System.out.println("Inserisci il tuo indirizzo di posta elettronica:");
             String EmailAddress=scan.nextLine();
+            LoginSession.add(EmailAddress);
+            
     
             if (EmailAddress.indexOf('@')==-1){
-                System.out.println("Indirizzo di posta elettronica inserito non valido");
+                System.out.println("Indirizzo di posta elettronica inserito non valido\n");
                 wait(3500);
                 break;
             }
@@ -82,11 +96,12 @@ public class Registrazione {
             try{        
                 System.out.println("Inserisci il tuo UserdId:");
                 String UserId=scan.nextLine();
+                LoginSession.add(UserId);
     
     
             }
             catch(Exception e) {
-                System.out.println("UserId inserito non valido");
+                System.out.println("UserId inserito non valido\n");
                 wait(3500);
                 break;
             }
@@ -94,15 +109,47 @@ public class Registrazione {
             //Inserimento Password      
             System.out.println("Inserisci la tua password:\n(Minimo 7 caratteri)");
             String UserPassword=scan.nextLine();
+            LoginSession.add(UserPassword);
     
     
             if (UserPassword.length()<6)
-                System.out.println("Password inserita non valida");
+                System.out.println("Password inserita non valida\n");
                 wait(3500);
                 break;
             }
 
-            //Salvataggio dati nel csv + return 
+            if (LoginSession.size()==7){
+
+                try {
+                    //Salvataggio dati nel csv + return 
+                    File file=new File("UtentiRegistrati.dati.csv" );
+                            // create FileWriter object with file as parameter
+                    FileWriter outputfile = new FileWriter(file, true);
+            
+                    // create CSVWriter object filewriter object as parameter
+                    CSVWriter writer = new CSVWriter(outputfile);         
+                    String [] CsvLogin={LoginSession.get(0),LoginSession.get(1),LoginSession.get(2),LoginSession.get(3),LoginSession.get(4),LoginSession.get(5),LoginSession.get(6)} ;
+                    writer.writeNext(CsvLogin);
+                    // Now clear the stream
+                    // using flush() method
+                    writer.flush();
+                    writer.close();
+                    
+                    return true;
+                    }
+                    catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+            }
+            else{
+                return false;
+            }    
+
+
+            return false;
+            
+
         }
 
 }
