6c2123c3360df52097505c62e4a5d8aed98b8a23
Emanuele Buggin <emanuelebuggin@gmail.com>
2022-05-17 14:20:11 +0200

    Modifica
    Switch case al posto di if else

--- Reader.java
+++ Reader.java
@@ -20,53 +20,62 @@ public class Reader {
         System.out.println("ricerca per Brano(0) o per Autore(1)");
         int scelta = scanner.nextInt();
         
-        if(scelta == 0){
-            try {
-                BufferedReader buffer = new BufferedReader(new FileReader(path));
-                System.out.println("Stringa da ricercare");
-                String cerca = scanner.next();
-
-                while((riga = buffer.readLine()) != null){
-                    int colonna = 0;
-                    String[] valoriLetti = riga.split(",");
-                    if(valoriLetti[colonna].contains(cerca)){
-                        System.out.println(valoriLetti[colonna]);
-                        trovato = true;
+        switch(scelta){
+            case 0: {
+                try {
+                    BufferedReader buffer = new BufferedReader(new FileReader(path));
+                    System.out.println("Stringa da ricercare");
+                    String cerca = scanner.next();
+    
+                    while((riga = buffer.readLine()) != null){
+                        int colonna = 0;
+                        String[] valoriLetti = riga.split(",");
+                        if(valoriLetti[colonna].contains(cerca)){
+                            System.out.println(valoriLetti[colonna]);
+                            trovato = true;
+                        }
                     }
-                }
-                buffer.close();
-                }catch (FileNotFoundException e) {
-                    System.out.println("File non trovato");
-                    e.printStackTrace();  
-                }catch(IOException e){
-                    e.printStackTrace();
-                }
-        }else if(scelta == 1){
-            try {
-                BufferedReader buffer = new BufferedReader(new FileReader(path));
-                System.out.println("Stringa da ricercare");
-                String cerca = scanner.next();
-
-                while((riga = buffer.readLine()) != null){
-                    int colonna = 1;
-                    String[] valoriLetti = riga.split(",");
-                    if(valoriLetti[colonna].contains(cerca)){
-                        System.out.println(valoriLetti[colonna]);
-                        trovato = true;
+                    buffer.close();
+                    }catch (FileNotFoundException e) {
+                        System.out.println("File non trovato");
+                        e.printStackTrace();  
+                    }catch(IOException e){
+                        e.printStackTrace();
                     }
+                break;
+            }
+
+            case 1:{
+                try {
+                    BufferedReader buffer = new BufferedReader(new FileReader(path));
+                    System.out.println("Stringa da ricercare");
+                    String cerca = scanner.next();
+    
+                    while((riga = buffer.readLine()) != null){
+                        int colonna = 1;
+                        String[] valoriLetti = riga.split(",");
+                        if(valoriLetti[colonna].contains(cerca)){
+                            System.out.println(valoriLetti[colonna]);
+                            trovato = true;
+                        }
+                    }
+                    buffer.close();
+                    }catch (FileNotFoundException e) {
+                        System.out.println("File non trovato");
+                        e.printStackTrace();  
+                    }catch(IOException e){
+                        e.printStackTrace();
+                    }
+            }
+
+            default:{
+                if(trovato == false){
+                    System.out.println("elemento non trovato");
                 }
-                buffer.close();
-                }catch (FileNotFoundException e) {
-                    System.out.println("File non trovato");
-                    e.printStackTrace();  
-                }catch(IOException e){
-                    e.printStackTrace();
-                }
+            }
         }
 
-        if(trovato == false){
-            System.out.println("elemento non trovato");
-        }
+
         
         }
         
